9fed12fdea75fe2f32eb90c78a485ded543d0ed4
Kurt Peters <T-800@Kurts-MacBook-Pro.local>
2014-11-12 08:46:28 -0600

    additions: pluralization, attribute settings, expression default

--- src/diet-i18n.js
+++ src/diet-i18n.js
@@ -1,47 +1,155 @@
 (function(root) {
     'use strict';
 
-    var i18n, phraseRegistry = {}, defaultText = 'i18n_void: ';
+    var attributes = {},
+        pluralizationRule,
+        variants;
 
-    root.i18n = i18n = function(path, expressions) {
+    variants = { // l: locales; r: rule
+        "zh": { // chinese
+            "l": ['fa', 'id', 'ja', 'ko', 'lo', 'ms', 'th', 'tr', 'zh'],
+            "r": function(n) { return 0; }
+        },
+        "de": { // german
+            "l": ['da', 'de', 'en', 'es', 'fi', 'gb', 'el', 'he', 'hu', 'it', 'nl', 'no', 'pt', 'sv', 'uk'],
+            "r": function(n) { return n !== 1 ? 1 : 0; }
+        },
+        "fr": { // french
+            "l": ['fr', 'tl', 'pt-br'],
+            "r": function(n) { return n > 1 ? 1 : 0; }
+        },
+        "ru": { // russian
+            "l": ['hr', 'ru'],
+            "r": function(n) { return n % 10 === 1 && n % 100 !== 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2; }
+        },
+        "cs": { // czech
+            "l": ['cs'],
+            "r": function(n) { return (n === 1) ? 0 : (n >= 2 && n <= 4) ? 1 : 2; }
+        },
+        "pl": { // polish
+            "l": ['pl'],
+            "r": function(n) { return (n === 1 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2); }
+        },
+        "is": { // icelandic
+            "l": ['is'],
+            "r": function(n) { return (n % 10 !== 1 || n % 100 === 11) ? 1 : 0; }
+        }
+    };
 
-        var phrase = phraseRegistry[path] || defaultText + path,
-            expression,
-            regExp;
-        
-        for (expression in expressions) {
-            regExp = new RegExp('\\{\\{' + expression + '\\}\\}', 'g');
-            phrase = phrase.replace(regExp, expressions[expression]);
+    function trim(phrase) {
+        var regExp = /^\s+|\s+$/g;
+        return phrase.replace(regExp, '');
+    }
+
+    function setPluralizationRule() {
+
+        var locale = attributes.locale,
+            variant,
+            index;
+
+        for (variant in variants) {
+            variant = variants[variant];
+            index = variant.l.length;
+            while (index--) {
+                if (variant.l[index] === locale) {
+                    pluralizationRule = variant.r;
+                    return;
+                }
+            }
+        }
+    }
+
+    function pluralize(phrase, expressions) {
+
+        var property, rule;
+
+        if (~phrase.indexOf(attributes.delimiter)) {
+            phrase = phrase.split(attributes.delimiter);
+            if (!isNaN(expressions)) {
+                rule = +expressions;
+            } else {
+                for (property in expressions) {
+                    if (!isNaN(expressions[property])) {
+                        rule = +expressions[property];
+                        break;
+                    }
+                }
+            }
+
+            phrase = phrase[pluralizationRule(rule || 0)];
         }
 
-        return phrase;
-    };
+        return trim(phrase);
+    }
+
+    function i18n(path, expressions) {
+
+        var phrase = attributes.registry[path] || (expressions ? expressions.__default__ : null);
+
+        if (phrase != null && phrase.constructor === String) {
+            phrase = phrase.replace(attributes.expression, function(expression, property) {
+                return expressions[property] || '';
+            });
+            phrase = pluralize(phrase, expressions);
+
+        } else {
+            phrase = null;
+        }
+
+        return phrase || (attributes.allowVoid ? '' : attributes.defaultText + path);
+    }
 
     i18n.extend = function(phrases, prefix) {
 
-        var item, phrase;
+        var item,
+            phrase;
 
         for (item in phrases) {
             phrase = phrases[item];
-
             if (prefix !== void 0) {
-                item = prefix + '.' + item;
+                item = prefix + attributes.notation + item;
             }
-
             if (phrase instanceof Object) {
                 this.extend(phrase, item);
                 continue;
             }
-
-            phraseRegistry[item] = phrase;
+            attributes.registry[item] = phrase;
         }
 
         return this;
     };
 
-    i18n.restore = function() {
-        phraseRegistry = {};
+    i18n.set = function(properties) {
+
+        for (var property in properties) {
+            attributes[property] = properties[property];
+            if (property === 'locale') {
+                setPluralizationRule();
+            }
+        }
+
         return this;
     };
-    
+
+    i18n.get = function(property) {
+        return attributes[property];
+    };
+
+    i18n.empty = function() {
+        this.set({
+            "registry": {}
+        });
+        return this;
+    };
+
+    root.i18n = i18n.set({ // Default Configuration
+        "allowVoid": false,
+        "defaultText": 'i18n_void: ',
+        "delimiter": '||',
+        "expression": /%\{(.*?)\}/g,
+        "locale": 'en',
+        "notation": '.',
+        "registry": {}
+    });
+
 })(this);
--- tests/diet-i18n.spec.js
+++ tests/diet-i18n.spec.js
@@ -13,13 +13,23 @@ describe('i18n', function() {
     });
 
     it('should apply expression handling for desired translation', function() {
-        var phrase = {"test": "this is a mocha {{expression}}"};
+        var phrase = {"test": "this is a mocha %{expression}"};
         i18n.extend(phrase);
         expect(i18n('test', {"expression": 'test'})).to.equal('this is a mocha test');
-        phrase = {"test": "this is a {{thing}} {{expression}}"};
+        phrase = {"test": "this is a %{thing} %{expression}"};
         i18n.extend(phrase);
         expect(i18n('test', {"expression": 'practice', "thing": 'TDD'})).to.equal('this is a TDD practice');
         expect(i18n('tset', {"expression": 'practice', "thing": 'TDD'})).to.equal('i18n_void: tset');
+        phrase = {"test": "%{expression} this, %{expression} that."};
+        i18n.extend(phrase);
+        expect(i18n('test', {"expression": 'Mocha'})).to.equal('Mocha this, Mocha that.');
+    });
+
+    it('should use expression default if one is populated', function() {
+        expect(i18n('test')).to.equal('i18n_void: test');
+        expect(i18n('test', {
+            "__default__": 'test content'
+        })).to.equal('test content');
     });
 
     it('should register translations by extending the current registry', function() {
@@ -34,13 +44,40 @@ describe('i18n', function() {
         };
         i18n.extend(set);
         expect(i18n('name')).to.equal(set.name);
-        expect(i18n('data.one')).to.equal(set.data.one);
+        expect(i18n('data.one')).to.equal('i18n_void: data.one');
         expect(i18n('data.more.moreData')).to.equal(set.data.more.moreData);
         expect(i18n('data.more.moredata')).to.equal('i18n_void: data.more.moredata');
     });
 
+    it('should empty out phrase registry', function() {
+        i18n.extend({
+            "test": 'this is a test'
+        });
+        expect(i18n('test')).to.equal('this is a test');
+        i18n.empty();
+        expect(i18n('test')).to.equal('i18n_void: test');
+    });
+
+    it('should determine pluralized context for translation', function() {
+        i18n.extend({
+            "test": 'orange || oranges'
+        });
+        expect(i18n('test', 1)).to.equal('orange');
+        expect(i18n('test', '1')).to.equal('orange');
+        expect(i18n('test', 2)).to.equal('oranges');
+        expect(i18n('test', '2')).to.equal('oranges');
+        expect(i18n('test', {"n": 2})).to.equal('oranges');
+    });
+
+    it('should remove error messaging if void content is allowed', function() {
+        expect(i18n('test')).to.equal('i18n_void: test');
+        i18n.set({"allowVoid": true});
+        expect(i18n('test')).to.equal('');
+        i18n.set({"allowVoid": false});
+    });
+
     afterEach(function() {
-        i18n.restore();
+        i18n.empty();
     });
 
 });
